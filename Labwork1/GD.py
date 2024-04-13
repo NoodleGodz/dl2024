ba091e63fae1a17d7cee79a03027914840e499bc
NoodleGodz <92013756+NoodleGodz@users.noreply.github.com>
2024-04-13 16:13:21 +0700

    first labwork

---
+++ Labwork1/GD.py
@@ -0,0 +1,23 @@
+
+def f(x):
+    return x**2
+
+def f_(x):
+    return 2*x
+
+
+
+def gradient_d(x,L,stop):
+    time = 0
+    print(f"{time}\t{x:.3f}\t{f(x):.3f}")
+    while f(x)>stop:
+        time = time +1
+        x = x - L * f_(x)
+        print(f"{time}\t{x:.3f}\t{f(x):.3f}")
+
+
+x0 = -2
+L = 1
+stop = 0.001
+print(f"time\tx\tf(x)")
+gradient_d(x0,L,stop)
