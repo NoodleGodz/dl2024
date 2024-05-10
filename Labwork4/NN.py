3eb4946829b9bef444ca578af03b686c33b5064b
NoodleGodz <92013756+NoodleGodz@users.noreply.github.com>
2024-05-10 21:21:35 +0700

    new stuffs here

--- Labwork4/NN.py
+++ Labwork4/NN.py
@@ -11,7 +11,7 @@ class Neural_Node:
     def __init__(self, nout) -> None:
         self.nout = nout
         self.weight = []
-        self.value = 1
+        self.value = 0
 
     def random_mode(self) -> None:
         for _ in range(self.nout):
@@ -20,19 +20,14 @@ class Neural_Node:
     def input_mode(self, w):
         self.weight = w
         
-    
-    def update(self, x):
-        self.value = sigmoid(x)
-
     def raw_update(self, x):
         self.value = x
 
     def __repr__(self) -> str:
-        return str(self.weight)
+        return f"Weight: {self.weight}, Value: {self.value:.2f}"
 
 
 class Layer:
-    finalNode = None
     def __init__(self, numofNeu, numofNext) -> None:
         self.neuron = numofNeu
         self.next_layer = numofNext
@@ -56,47 +51,51 @@ class Layer:
             self.finalNode = a
 
     def __repr__(self) -> str:
-        return str([neuron.weight for neuron in self.neuronlist]) + " bias " + str(self.bias)
+        representation = f"Bias: {self.bias}\n"
+        for i, neuron in enumerate(self.neuronlist):
+            representation += f"Neuron {i+1}: {neuron}\n"
+        return representation
 
     def feed_forward(self, inputs):
         self.update_value(inputs)
-        #print(inputs)
         outputs = list(self.bias)
-        #print(self.neuronlist)
         for j, neuron in enumerate(self.neuronlist) :
             #print(neuron)
             w = neuron.weight
             for i in range(len(outputs)):
-                #print(i)
-                #print(f"{outputs[i]} += {w[i]}*{inputs[j]}")
                 outputs[i] += w[i]*inputs[j]
                 
         #print(outputs)
         for i in range(len(outputs)):
-            a=  sigmoid(outputs[i])
+            a = sigmoid(outputs[i])
             
             if a>0.5 :
                 outputs[i] = 1
             else : 
-                 outputs[i] = 0
-
-            #outputs[i]= a
+                outputs[i] = 0
 
 
-        
         return outputs
     
+    def backprop(self,weight):
+        pass
+
     def update_value(self, inputs):
         if len(inputs)>0:
             for i, neuron in enumerate(self.neuronlist):
                 neuron.raw_update(inputs[i])
+
+    def last_layer_mode(self):
+        self.bias = [1]
+        a = Neural_Node(1)
+        a.weight = [1]
+        self.neuronlist.append(a)
 
     
 
 class Neural_Net:
     def __init__(self, filename) -> None:
         self.nofneural = []
-        self.finalNode = None
         self.layer = {}
         self.random = self.read_textfile(filename=filename)
         for i in range(self.noflayer):
@@ -104,10 +103,11 @@ class Neural_Net:
             if self.random:
                 a.random_mode()
             else: 
-                if i != self.noflayer - 1: 
+                if i != self.noflayer -1: 
                     a.input_mode(self.w[i])
+                else:
+                    a.last_layer_mode()
                 self.layer[f"Layer {i}"] = a 
-                self.finalNode = a.finalNode
 
     def read_textfile(self, filename):
         with open(file=filename, mode="r+") as f:
@@ -125,20 +125,31 @@ class Neural_Net:
 
     def feed_forward(self, inputs):
         output = inputs
-        for i in range(self.noflayer-1):
+        for i in range(self.noflayer -1):
             output = self.layer[f"Layer {i}"].feed_forward(output)
+        self.layer[f"Layer {i+1}"].update_value(output)
         return output
-
-nn = Neural_Net("Labwork4\\XOR.txt")
+    
+    def __repr__(self) -> str:
+        representation = ""
+        for name, layer in self.layer.items():
+            representation += f"{name}:\n{layer}\n"
+        return representation
+    
+v = "XOR"
+nn = Neural_Net(f"Labwork4\\{v}.txt")
 print(nn.layer)
 
 input_values = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
 
-print("Truth Table:")
+print(f"\nTruth Table: {v}")
 print("Input 1 | Input 2 | Predicted Output")
 print("-------------------------------------")
 for input_value in input_values:
     out = nn.feed_forward(input_value)
-    output = 1 if out[0] > 0.5 else 0
-    print(f"   {input_value[0]}    |    {input_value[1]}    |        {output}")
+    #output = 1 if out[0] > 0.5 else 0
+    print(f"   {input_value[0]}    |    {input_value[1]}    |        {out[0]}")
+
+
+print("\n",nn)
